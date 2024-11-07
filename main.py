d79d2f836011899e855ebde1116d49ea6db317b9
PhillGD <phillgd@outlook.com>
2024-11-07 14:33:02 +0000

    print data report

--- main.py
+++ main.py
@@ -3,9 +3,14 @@ def main():
     book_text = get_book_text(book_path)
     #print(book_text)
     word_count = count_words(book_text)
-    #print(f"Word Count: {word_count}")
     chars_dict = get_chars_dict(book_text)
-    print(f"Character Dictionary: {chars_dict}")
+    print(f"--- Begin Report of {book_path} ---")
+    print(f"Word Count: {word_count}")
+    print("Character Count:")
+    for char in sorted(chars_dict, key=chars_dict.get, reverse=True):
+        if char.isalpha():
+            print(f"'{char}' : {chars_dict[char]}")
+    print("--- End Report ---")
 
 def get_book_text(path):
     with open(path) as f:
